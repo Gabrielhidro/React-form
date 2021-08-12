eb3e5f60d069e38e8aa97f0c4b94d3564596acac
Gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-12 12:26:23 -0300

    Import material UI lib

--- src/components/FormulárioCadastro/FormularioCadastro.jsx
+++ src/components/FormulárioCadastro/FormularioCadastro.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { Button } from "@material-ui/core";
 
 export function FormularioCadastro() {
   return (
     <form>
+      <label htmlFor="">Nome</label>
       <input type="text" name="" id="" />
+
+      <label htmlFor="">Sobrenome</label>
       <input type="text" name="" id="" />
+
+      <label htmlFor="">CPF</label>
       <input type="text" name="" id="" />
+
+      <label htmlFor="">Promoções</label>
+      <input type="checkbox" name="" id="" />
+
+      <label htmlFor="">Novidades</label>
+      <input type="checkbox" name="" id="" />
+
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
     </form>
   );
 }
