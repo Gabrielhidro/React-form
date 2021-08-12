46a91c73e9136b1be66e6614ec0e1899790f21cb
Gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-12 12:18:48 -0300

    Clean archives

---
+++ src/components/FormulárioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function FormularioCadastro() {
+  return (
+    <form>
+      <input type="text" name="" id="" />
+      <input type="text" name="" id="" />
+      <input type="text" name="" id="" />
+    </form>
+  );
+}
