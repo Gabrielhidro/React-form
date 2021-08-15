82648e889bf28e3d975ef25f3f5271cfaccaaa9a
Gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-15 15:32:10 -0300

    Create dadosDeEntrega component

---
+++ src/components/FormulárioCadastro/DadosDeEntrega.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Button, TextField } from "@material-ui/core";
+
+export function DadosDeEntrega() {
+  return (
+    <form>
+      <TextField
+        id="CEP"
+        label="CEP"
+        type="number"
+        color="secondary"
+        variant="outlined"
+        margin="dense"
+        fullWidth
+      />
+
+      <TextField
+        id="Endereço"
+        label="Endereço"
+        type="text"
+        color="secondary"
+        variant="outlined"
+        margin="dense"
+        fullWidth
+      />
+
+      <TextField
+        id="numero"
+        label="Número"
+        type="number"
+        color="secondary"
+        variant="outlined"
+        margin="dense"
+      />
+
+      <TextField
+        id="estado"
+        label="Estado"
+        type="text"
+        color="secondary"
+        variant="outlined"
+        margin="dense"
+      />
+
+      <TextField
+        id="cidade"
+        label="Cidade"
+        type="text"
+        color="secondary"
+        variant="outlined"
+        margin="dense"
+        fullWidth
+      />
+
+      <Button type="submit" variant="contained" color="primary">
+        Finalizar Cadrasto
+      </Button>
+    </form>
+  );
+}
--- src/components/FormulárioCadastro/FormularioCadastro.jsx
+++ src/components/FormulárioCadastro/FormularioCadastro.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
+import { DadosDeEntrega } from "./DadosDeEntrega";
 import { DadosPessoais } from "./DadosPessoais";
-import { DadosUsuario } from "./DdosUsuario";
+import { DadosUsuario } from "./DadosUsuario";
 
 export function FormularioCadastro({ onSubmit, validations }) {
   return (
     <>
       <DadosPessoais onSubmit={onSubmit} validations={validations} />
       <DadosUsuario />
+      <DadosDeEntrega />
     </>
   );
 }
