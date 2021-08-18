c5ad54109337da51f1bf066ffc0033edae4bb688
gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-18 20:13:30 -0300

    Create useffect and state for form datas

--- src/components/FormulárioCadastro/FormularioCadastro.jsx
+++ src/components/FormulárioCadastro/FormularioCadastro.jsx
@@ -1,17 +1,27 @@
 import { Typography } from "@material-ui/core";
 import React, { useState } from "react";
+import { useEffect } from "react";
 import { DadosDeEntrega } from "./DadosDeEntrega";
 import { DadosPessoais } from "./DadosPessoais";
 import { DadosUsuario } from "./DadosUsuario";
 
 export function FormularioCadastro({ onSubmit, validations }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
+  const [ datas, setDatas ] = useState({})
+  useEffect(() => {
+    console.log(datas);
+  })
 
   const forms = [
-    <DadosUsuario onSubmit={next} />,
-    <DadosPessoais onSubmit={next} validations={validations} />,
-    <DadosDeEntrega onSubmit={onSubmit} />,
+    <DadosUsuario onSubmit={getDatasfunc} />,
+    <DadosPessoais onSubmit={getDatasfunc} validations={validations} />,
+    <DadosDeEntrega onSubmit={getDatasfunc} />,
   ];
+
+  function getDatasfunc(datas){
+    setDatas({...datas, ...datas})
+    next();
+  }
 
   function next() {
     setEtapaAtual(etapaAtual + 1);
